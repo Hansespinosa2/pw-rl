bf5c38f9bcbc8d9f8603a50561477b5d2c360bf1
Hansespinosa2 <andresespinosa@ufl.edu>
2024-04-04 23:07:16 -0400

    Add screenshot capability to the run_emulator

--- run_emulator.py
+++ run_emulator.py
@@ -1,11 +1,17 @@
 from desmume.emulator import DeSmuME
+import os
 
 emu = DeSmuME()
 emu.open('emu/pw-aa.nds')
 
 window = emu.create_sdl_window()
-
+i = 0
 while not window.has_quit():
     window.process_input()
     emu.cycle()
     window.draw()
+    i+=1
+    if i%1000 == 0:
+        screenshot = emu.screenshot()
+        screenshot.save(f'screenshots/screenshot_{emu.get_ticks()}.png')
+
